0e8c7f007c2196f0e7cda432ffdfea16fb9e1647
Vitaliy Scheremet <shervit82@gmail.com>
2019-06-03 10:44:26 +0300

    fstc

---
+++ script.js
@@ -0,0 +1,24 @@
+window.onload = function () {
+    
+let mainBtn = document.getElementById('themeChange');
+if (localStorage.getItem('bgcolor') !== null){
+    let colorBg = localStorage.getItem('bgcolor');
+    document.body.style.backgroundColor = colorBg;
+}
+
+mainBtn.addEventListener("click", function() {
+    const curColour = document.body.style.backgroundColor;
+   
+    if (curColour === 'limegreen') {
+        document.body.style.backgroundColor = '';
+        localStorage.setItem('bgcolor','')
+        }
+    else {
+        document.body.style.backgroundColor = 'limegreen';
+        localStorage.setItem('bgcolor','limegreen')
+        
+    }
+ });
+};
+
+
